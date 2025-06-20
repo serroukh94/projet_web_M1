49652acca363e7c4c06a943e47f119097d713f7a
Ben Tama Serroukh <88806891+serroukh94@users.noreply.github.com>
2025-06-20 22:36:39 +0200

    Remove unused mongoose import

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as mongoose from 'mongoose';         
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
